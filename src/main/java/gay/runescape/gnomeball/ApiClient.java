286ef3df202ced5f8b7c5e46f6ad139395fdd1ca
codycrossley <cody@codycrossley.com>
2026-08-01 13:25:31 -0400

    Point API to Production Server

--- src/main/java/gay/runescape/gnomeball/ApiClient.java
+++ src/main/java/gay/runescape/gnomeball/ApiClient.java
@@ -10,8 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ApiClient
 {
-    //static final String BASE_URL = "https://gnomeball.runescape.gay/gnomeball";
-    static final String BASE_URL = "http://localhost:8002/gnomeball";
+    static final String BASE_URL = "https://gnomeball.runescape.gay/gnomeball";
+    //static final String BASE_URL = "http://localhost:8002/gnomeball";
 
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
