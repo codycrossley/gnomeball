6bc94e6ce6be605bd1b29496d53c0a40b8673347
codycrossley <cody@codycrossley.com>
2026-08-14 11:48:39 -0400

    runescape.gay -> shrunk.studio

--- src/main/java/gay/runescape/gnomeball/ApiClient.java
+++ src/main/java/gay/runescape/gnomeball/ApiClient.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ApiClient
 {
-    static final String BASE_URL = "https://gnomeball.runescape.gay/gnomeball";
+    static final String BASE_URL = "https://gnomeball.shrunk.studio/gnomeball";
     //static final String BASE_URL = "http://localhost:8002/gnomeball";
 
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
